dccf92ee427148d75331c4d6f6489ac4527c4ad9
michelemilazzo <michele.milazzo@live.com>
2026-01-25 15:01:09 +0100

    Prepare eu_einvoice for MMOS marketplace

---
+++ hooks.py
@@ -0,0 +1,9 @@
+
+app_name = "eu_einvoice"
+app_title = "European e-Invoice"
+app_publisher = "MMOS Marketplace"
+app_description = "European e-Invoice"
+app_icon = "octicon octicon-star"
+app_color = "#f48024"
+app_email = "developers@frappe.io"
+app_license = "MIT"
